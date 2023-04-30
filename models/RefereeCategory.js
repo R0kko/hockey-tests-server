529ac9ac0af2b1b0c929c422f54e983589bad325
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:20:33 +0300

    models/RefereeCategory.js initialization

---
+++ models/RefereeCategory.js
@@ -0,0 +1,47 @@
+const {Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = require('../config/database');
+
+class RefereeCategory extends Model {
+}
+
+RefereeCategory.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        category_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: 'category_names',
+                key: 'id',
+            },
+        },
+        assigned_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.NOW,
+        },
+
+        valid_until: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'RefereeCategory',
+    }
+);
+
+module.exports = RefereeCategory;
