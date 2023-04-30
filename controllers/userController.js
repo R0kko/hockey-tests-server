59ad97baa4e52a4ebf22ce428d5cb037c6faec44
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:19:00 +0300

    routes/userController.js initialization

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    // Implement registration logic
+};
+
+exports.login = async (req, res) => {
+    // Implement login logic
+};
+
+exports.getUser = async (req, res) => {
+    // Implement logic to get a user by ID
+};
+
+exports.updateUser = async (req, res) => {
+    // Implement logic to update a user
+};
+
+exports.deleteUser = async (req, res) => {
+    // Implement logic to delete a user
+};
