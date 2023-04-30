5e62b9b4826fcf50fd9cdb5460c7283ff6cee5e4
Alexey Drobot <alexey.drobot@outlook.com>
2023-05-01 00:53:46 +0300

    login implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {Op} = require("sequelize");
 
 exports.register = async (req, res) => {
@@ -55,7 +56,48 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    // Implement login logic
+    try {
+        const { username, password } = req.body;
+
+        console.log(username);
+
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
+
+        const user = await User.findOne({ where: { username } });
+
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user.id, role_id: user.role_id }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        });
+
+        res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                patronymic: user.patronymic,
+                role_id: user.role_id,
+            },
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 exports.getUser = async (req, res) => {
