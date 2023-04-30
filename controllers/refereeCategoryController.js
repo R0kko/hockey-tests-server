dd2aa6d872856c73cce6c31e1ba8016771a19da8
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:22:03 +0300

    controllers/refereeCategoryController.js initialization

---
+++ controllers/refereeCategoryController.js
@@ -0,0 +1,21 @@
+const RefereeCategory = require('../models/RefereeCategory');
+
+exports.assignCategory = async (req, res) => {
+    // Implement logic to assign a category to a referee
+};
+
+exports.getAllRefereeCategories = async (req, res) => {
+    // Implement logic to get all referee categories
+};
+
+exports.getRefereeCategory = async (req, res) => {
+    // Implement logic to get a referee category by ID
+};
+
+exports.updateRefereeCategory = async (req, res) => {
+    // Implement logic to update a referee category
+};
+
+exports.deleteRefereeCategory = async (req, res) => {
+    // Implement logic to delete a referee category
+};
