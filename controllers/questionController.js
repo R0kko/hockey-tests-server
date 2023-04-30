de504bea5e484c5146fee1e49c15fd50b6b1522b
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:21:41 +0300

    controllers/questionController.js initialization

---
+++ controllers/questionController.js
@@ -0,0 +1,36 @@
+const Question = require('../models/Question');
+
+exports.createQuestion = async (req, res) => {
+    // Implement logic to create a question
+};
+
+exports.getAllQuestions = async (req, res) => {
+    // Implement logic to get all questions
+};
+
+exports.getQuestion = async (req, res) => {
+    // Implement logic to get a question by ID
+};
+
+exports.updateQuestion = async (req, res) => {
+    // Implement logic to update a question
+};
+
+module.exports = {
+    deleteQuestion: (req, res) => {
+    },
+
+    createQuestion: (req, res) => {
+    },
+
+    getAllQuestions: (req, res) => {
+    },
+
+    getQuestion: (req, res) => {
+    },
+
+    updateQuestion: (req, res) => {
+
+    }
+};
+
