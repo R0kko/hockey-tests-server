d6fd782b350c89205a0c8ffd96ced7f5f454ab6f
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:24:37 +0300

    app.js corrections (initialization)

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+const userRoutes = require('./routes/users');
+const questionRoutes = require('./routes/questions');
+const testRoutes = require('./routes/tests');
+const refereeCategoryRoutes = require('./routes/refereeCategories');
+
+app.use('/api/users', userRoutes);
+app.use('/api/questions', questionRoutes);
+app.use('/api/tests', testRoutes);
+app.use('/api/referee-categories', refereeCategoryRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
