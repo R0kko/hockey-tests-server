7878111d36d2323f60e863df0096ab28cc560d67
Alexey Drobot <alexey.drobot@outlook.com>
2023-04-30 22:22:13 +0300

    controllers/testController.js initialization

---
+++ controllers/testController.js
@@ -0,0 +1,21 @@
+const Test = require('../models/Test');
+
+exports.createTest = async (req, res) => {
+    // Implement logic to create a test
+};
+
+exports.getAllTests = async (req, res) => {
+    // Implement logic to get all tests
+};
+
+exports.getTest = async (req, res) => {
+    // Implement logic to get a test by ID
+};
+
+exports.updateTest = async (req, res) => {
+    // Implement logic to update a test
+};
+
+exports.deleteTest = async (req, res) => {
+    // Implement logic to delete a test
+};
